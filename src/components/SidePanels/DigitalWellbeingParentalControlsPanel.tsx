e91e3d14322235f4efaa4e65bbba9ef0b58a8a53
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-22 14:06:40 +0530

    Add parental control  panel

--- src/components/SidePanels/DigitalWellbeingParentalControlsPanel.tsx
+++ src/components/SidePanels/DigitalWellbeingParentalControlsPanel.tsx
@@ -1,9 +1,14 @@
+import { Preloader } from "konsta/react"
+
 const DigitalWellbeingParentalControlsPanel = () => {
   return (
     <div className="flex-col items-start mt-3 pl-2">
       <h1 className="pl-2  font-light text-[34px]">
         Digital Wellbeing and parental controls{" "}
       </h1>
+      <div className="flex flex-col items-center justify-center h-[650px] font-light text-4xl mb-6">
+        <Preloader />
+      </div>
     </div>
   )
 }
