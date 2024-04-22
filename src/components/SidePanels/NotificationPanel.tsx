b6054acccdbd36fd63d4d5452a84a91c65e26d5d
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-22 12:17:21 +0530

    Replace floating icon

--- src/components/SidePanels/NotificationPanel.tsx
+++ src/components/SidePanels/NotificationPanel.tsx
@@ -1,5 +1,6 @@
 import { CgHomeScreen } from "react-icons/cg"
 import { MdScreenLockPortrait } from "react-icons/md"
+import { HiMiniDevicePhoneMobile } from "react-icons/hi2"
 
 import { Chip } from "konsta/react"
 function NotificationPanel() {
@@ -14,7 +15,7 @@ function NotificationPanel() {
         </div>
         <div>
           <div className="h-32 w-32 flex flex-col items-center space-y-2">
-            <CgHomeScreen size={104} />
+            <HiMiniDevicePhoneMobile size={104} />
             <Chip className="m-0.5 text-sky-600 bg-sky-400">Floating</Chip>
           </div>
         </div>
